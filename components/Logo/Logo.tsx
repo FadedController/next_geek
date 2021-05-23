832d386d171115e8483de500394ea0edf9f71488
FadedController <adpadillar25@gmail.com>
2021-05-23 10:06:33 -0600

    Fixed logo - Fixed sm screen bug

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -4,11 +4,12 @@ const Logo = (): JSX.Element => {
   const router = useRouter();
   return (
     <div
-      className="flex text-xl py-4 space-x-1 cursor-pointer"
+      className="flex text-xl space-x-1 cursor-pointer"
       onClick={() => router.push("#")}
     >
-      <h1 className="font-semibold text-blue-700">Next</h1>
-      <h2 className="font-normal text-yellow-500">Geek</h2>
+      <img src="logo.jpeg" alt="logo" className="h-16" />
+      {/* <h1 className="font-semibold text-blue-700">Next</h1>
+      <h2 className="font-normal text-yellow-500">Geek</h2> */}
     </div>
   );
 };
